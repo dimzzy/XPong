7b5611a6749ee176361bd64f277595b8876469ed
Dmitry Stadnik <dimzzy@gmail.com>
2012-03-01 15:19:07 +0100

    Initial Commit

---
+++ XPong/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  XPong
+//
+//  Created by Stadnik Dmitry on 3/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
